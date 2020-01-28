785f0e18df9397cc9c62f66ebcd0ee1d98733ee4
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-28 15:24:28 -0600

    cleaned up the code so it looks organized.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -6,10 +6,7 @@ const Movie = props => {
   const [movie, setMovie] = useState();
 
   useEffect(() => {
-    // const id = 1;
     const id = props.match.params.id;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
 
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
@@ -21,12 +18,6 @@ const Movie = props => {
       });
   }, []);
 
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
-
   const saveMovie = () => {
     const addToSavedList = props.addToSavedList;
     addToSavedList(movie);
@@ -36,7 +27,6 @@ const Movie = props => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
       <MovieCard key={movie.id} movie={movie} />
